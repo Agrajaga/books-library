0a1ed90e994daaf9becb64d322ad699869a05b22
Michael <soplomiha@gmail.com>
2022-04-20 22:12:44 +0300

    Download books with titles

--- load_book.py
+++ load_book.py
@@ -1,19 +1,59 @@
 import os
-import requests
+import urllib.parse
+
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
+from requests import HTTPError, Response, get
 
 
+def check_for_redirect(response: Response) -> None:
+    if response.history:
+        raise HTTPError
+
+
+def download_txt(url, filename, folder='books/'):
+    """Функция для скачивания текстовых файлов.
+    Args:
+        url (str): Cсылка на текст, который хочется скачать.
+        filename (str): Имя файла, с которым сохранять.
+        folder (str): Папка, куда сохранять.
+    Returns:
+        str: Путь до файла, куда сохранён текст.
+    """
+    response = get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+
+    os.makedirs(folder, exist_ok=True)
+    filepath = os.path.join(folder, sanitize_filename(f"{filename}.txt"))
+
+    with open(filepath, "w") as txt_file:
+        txt_file.write(response.text)
+
+    return filepath
 
 
 if __name__ == "__main__":
-
+    os.makedirs("books", exist_ok=True)
 
     for index in range(1, 11):
-        params = {
-            "id": index,
-        }
-        response = requests.get("https://tululu.org/txt.php", params=params)
+        book_url = f"https://tululu.org/b{index}/"
+        response = get(book_url)
         response.raise_for_status()
+        try:
+            check_for_redirect(response)
+
+            soup = BeautifulSoup(response.text, "lxml")
+            caption_tag = soup.find(id="content").find("h1")
+            caption = caption_tag.text
+            title, author = map(str.strip, caption.split("::"))
+
+            params = {
+                "id": index,
+            }
+            txt_url = f"https://tululu.org/txt.php?{urllib.parse.urlencode(params)}"
+            filename = f"{index}. {title}"
+            download_txt(txt_url, filename)
+        except HTTPError:
+            print(f"HTTPError on {index}")
         
-        os.makedirs("books", exist_ok=True)
-        with open(f"books/id{index}.txt", "w") as f:
-            f.write(response.text)
